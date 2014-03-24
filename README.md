de11f4748a3c2e3471086f4583aedd761ee9c9a3
NoelPrint <jvcpremium@hotmail.fr>
2014-03-24 22:02:17 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,57 @@
+PyNoelShack
+===========
+
+A small Python module for NoelShack API.
+
+Description
+-----------
+
+This module can be used as a library from any other Python script, or
+as a command-line tool.
+
+Dependencies
+------------
+
+[Requests](http://python-requests.org/)
+
+
+Installation
+------------
+
+To install this script as a command-line tool, do the following, assuming
+you got a personal `bin` directory:
+
+```sh
+cp pynoelshack.py ~/bin/pynoelshack
+chmod +x ~/bin/pynoelshack
+```
+
+Examples
+--------
+
+### Python
+
+```python
+from pynoelshack import NoelShack, NoelShackError
+
+ns = NoelShack()
+
+try:
+    print(ns.upload('foo.png'))
+except NoelShackError as e:
+    print(str(e))
+```
+
+### Command
+
+```sh
+python pynoelshack.py foo.png
+```
+
+Or, assuming you installed it in your `PATH` as `pynoelshack`:
+
+```sh
+pynoelshack foo.png
+```
+
+Feel free to alias it at your convenience.
---
+++ pynoelshack.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+import os.path
+import re
+import requests
+import sys
+
+
+class NoelShackError(Exception):
+    pass
+
+
+class NoelShack:
+    API_URL = 'http://www.noelshack.com/api.php'
+
+    def upload(self, file):
+        with open(file, 'rb') as f:
+            r = requests.post(self.API_URL, files={'fichier': f})
+
+        if not 'www.noelshack.com' in r.text:
+            raise NoelShackError(r.text)
+
+        return self.parse(r.text)
+
+    def parse(self, url):
+        return re.sub(r'www\.noelshack\.com/([0-9]+)-([0-9]+)-(.+)',
+                      r'image.noelshack.com/fichiers/\1/\2/\3',
+                      url)
+
+
+def main(argv):
+    if 2 != len(argv):
+        print("Usage: {0} file".format(argv[0]), file=sys.stderr)
+        return 1
+
+    file = argv[1]
+
+    if not os.path.isfile(file):
+        print("{0}: No such file".format(file), file=sys.stderr)
+        return 1
+
+    ns = NoelShack()
+
+    try:
+        ns.upload(file)
+    except NoelShackError as e:
+        print(str(e), file=sys.stderr)
+        return 1
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
